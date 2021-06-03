67090cefafe30a4d39c25dafd9c93b830c8d470b
Twometer <twometer@outlook.de>
2021-06-03 12:48:42 +0200

    Initialize project

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+int main() {
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Simulation", nullptr, nullptr);
+    if (!window) {
+        glfwDestroyWindow(window);
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    glfwPollEvents();
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 100");
+
+    while (!glfwWindowShouldClose(window)) {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    ImGui_ImplGlfw_Shutdown();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+
+    return 0;
+}
